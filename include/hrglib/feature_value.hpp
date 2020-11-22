9b4fc0e0ae9f356fd9ce9521d792d438ade9bc7b
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-11-22 14:54:04 +0100

    fix old osx build by using boost::variant as necessary for feature_value

--- include/hrglib/feature_value.hpp
+++ include/hrglib/feature_value.hpp
@@ -2,8 +2,22 @@
 #include "hrglib/feature_list.hpp"
 #include "hrglib/feature_types.hpp"
 
-#include <variant>
+#if __has_include(<variant>)
+# include <variant>
+
+namespace hrglib { namespace detail { namespace variantimpl {
+using std::variant;
+using std::get;
+}}}
+#else
+# include <boost/variant.hpp>
+
+namespace hrglib { namespace detail { namespace variantimpl {
+using boost::variant;
+using boost::get;
+}}}
+#endif
 
 namespace hrglib {
-using feature_value = std::variant<HRGLIB_FEATURE_TYPE_LIST>;
+using feature_value = detail::variantimpl::variant<HRGLIB_FEATURE_TYPE_LIST>;
 }
